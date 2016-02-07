ae3674598f7fad738cb7462b9cda0033c35b1f7d
davidebbo <david.ebbo@microsoft.com>
2016-02-07 09:40:56 -0800

    Initial code from sample
    https://azure.microsoft.com/en-us/documentation/articles/event-hubs-csharp-ephcs-getstarted/

---
+++ EventHubsReceiver/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.ServiceBus.Messaging;
+
+namespace EventHubsReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = ConfigurationManager.AppSettings["eventHubConnectionString"];
+            string eventHubName = ConfigurationManager.AppSettings["eventHubName"];
+            string storageAccountName = ConfigurationManager.AppSettings["storageAccountName"];
+            string storageAccountKey = ConfigurationManager.AppSettings["storageAccountKey"];
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>().Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
---
+++ EventHubsSender/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+
+namespace EventHubsSender
+{
+    class Program
+    {
+        static string eventHubName = ConfigurationManager.AppSettings["eventHubName"];
+        static string connectionString = ConfigurationManager.AppSettings["eventHubConnectionString"];
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Ctrl-C to stop the sender process");
+            Console.WriteLine("Press Enter to start now");
+            Console.ReadLine();
+            SendingRandomMessages();
+        }
+
+        static void SendingRandomMessages()
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+            while (true)
+            {
+                try
+                {
+                    var message = Guid.NewGuid().ToString();
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, message);
+                    eventHubClient.Send(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+                    Console.ResetColor();
+                }
+
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
